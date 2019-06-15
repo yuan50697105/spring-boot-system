54d5b14dce634803f252a8cc2cab7753e6f74a22
袁恩志 <710575900@qq.com>
2019-06-15 14:28:05 +0800

    1.拼音工具类 2.shiro 简单集成demo 3.security 简单集成demo 目前shiro和security还没有数据库结合

---
+++ spring-boot-chinese2pinyin/src/main/java/com/yuan/springbootchinese2pinyin/SpringBootChinese2pinyinApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.springbootchinese2pinyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChinese2pinyinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChinese2pinyinApplication.class, args);
+    }
+
+}
---
+++ spring-boot-web-security/src/main/java/com/yuan/springbootwebsecurity/SpringBootWebSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.springbootwebsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebSecurityApplication.class, args);
+    }
+
+}
