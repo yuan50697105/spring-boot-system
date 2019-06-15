23a11a3e0ca1d79e4263752f8c16b2b174a6e44a
袁恩志 <710575900@qq.com>
2019-06-15 19:27:34 +0800

    1.拼音工具类 2.shiro 简单集成demo 3.security 简单集成demo 目前shiro和security还没有数据库结合

--- spring-boot-web-jpa/src/main/java/com/yuan/springbootwebjpa/commons/service/BaseSerivce.java
+++ spring-boot-web-jpa/src/main/java/com/yuan/springbootwebjpa/commons/service/BaseSerivce.java
@@ -1,9 +1,14 @@
 package com.yuan.springbootwebjpa.commons.service;
 
 import com.yuan.springbootwebjpa.commons.entity.BaseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author yuane
@@ -14,4 +19,28 @@ public interface BaseSerivce<T extends BaseEntity, ID extends Serializable> {
     T save(T t);
 
     Iterable<T> saveAll(Iterable<T> ts);
+
+    void delete(ID id);
+
+    @Transactional
+    void delete(ID... ids);
+
+    @Transactional
+    void delete(Iterable<ID> ids);
+
+    Optional<T> findById(ID id);
+
+    List<T> findAllById(Iterable<ID> ids);
+
+    Optional<T> findOneByExample(T t);
+
+    List<T> findAll();
+
+    List<T> findAll(T t);
+
+    List<T> findAll(T t, Sort sort);
+
+    Page<T> findAll(Pageable pageable);
+
+    Page<T> findAll(T t, Pageable pageable);
 }
--- spring-boot-web-jpa/src/main/java/com/yuan/springbootwebjpa/commons/service/impl/BaseSerivceImpl.java
+++ spring-boot-web-jpa/src/main/java/com/yuan/springbootwebjpa/commons/service/impl/BaseSerivceImpl.java
@@ -3,9 +3,16 @@ package com.yuan.springbootwebjpa.commons.service.impl;
 import com.yuan.springbootwebjpa.commons.entity.BaseEntity;
 import com.yuan.springbootwebjpa.commons.repository.BaseRepository;
 import com.yuan.springbootwebjpa.commons.service.BaseSerivce;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author yuane
@@ -24,5 +31,66 @@ public abstract class BaseSerivceImpl<T extends BaseEntity, ID extends Serializa
     @Override
     public Iterable<T> saveAll(Iterable<T> ts) {
         return getRepository().saveAll(ts);
+    }
+
+    @Override
+    public void delete(ID id) {
+        getRepository().deleteById(id);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    @Transactional
+    public void delete(ID... ids) {
+        delete(Arrays.asList(ids));
+    }
+
+    @Override
+    @Transactional
+    public void delete(Iterable<ID> ids) {
+        List<T> list = getRepository().findAllById(ids);
+        if (list.size() > 0) {
+            getRepository().deleteInBatch(list);
+        }
+    }
+
+    @Override
+    public Optional<T> findById(ID id) {
+        return getRepository().findById(id);
+    }
+
+    @Override
+    public List<T> findAllById(Iterable<ID> ids) {
+        return getRepository().findAllById(ids);
+    }
+
+    @Override
+    public Optional<T> findOneByExample(T t) {
+        return getRepository().findOne(Example.of(t));
+    }
+
+    @Override
+    public List<T> findAll() {
+        return getRepository().findAll();
+    }
+
+    @Override
+    public List<T> findAll(T t) {
+        return getRepository().findAll(Example.of(t));
+    }
+
+    @Override
+    public List<T> findAll(T t, Sort sort) {
+        return getRepository().findAll(Example.of(t), sort);
+    }
+
+    @Override
+    public Page<T> findAll(Pageable pageable) {
+        return getRepository().findAll(pageable);
+    }
+
+    @Override
+    public Page<T> findAll(T t, Pageable pageable) {
+        return getRepository().findAll(Example.of(t), pageable);
     }
 }
