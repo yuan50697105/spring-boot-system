f15b9a25a32ad9f70e49ffd77cfc530abac26954
袁恩志 <710575900@qq.com>
2019-06-25 19:00:41 +0800

    完善各个data access

---
+++ spring-boot-web-jpa/src/main/java/com/yuan/springbootwebjpa/commons/controller/BaseController.java
@@ -0,0 +1,25 @@
+package com.yuan.springbootwebjpa.commons.controller;
+
+import com.yuan.springbootwebjpa.commons.entity.dto.Result;
+import com.yuan.springbootwebjpa.commons.entity.vo.AjaxResult;
+import com.yuan.springbootwebjpa.system.entity.bo.SysUserQueryParam;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.WebAsyncTask;
+
+/**
+ * @author yuane
+ * @date 2019/6/25 18:36
+ **/
+@ControllerAdvice
+public abstract class BaseController {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public WebAsyncTask<AjaxResult> exceptionHandler(Exception e) {
+        return new WebAsyncTask<>(() -> Result.of(Result.Status.ERROR, e.getMessage(), null).toAjax());
+    }
+
+
+    public abstract WebAsyncTask<AjaxResult> dataList(SysUserQueryParam queryParam);
+}
