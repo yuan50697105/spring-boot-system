66d6f9484c1b483f11a37445539986bb2dd36c5d
袁恩志 <710575900@qq.com>
2019-07-10 19:14:15 +0800

    统一命名 dao-xxx为BaseDao和BaseService web-xxx为MVC、Flux和视图部分 mq-xxx为消息中间件部分 shiro与security为安全鉴权部分 task为定时任务与批处理

---
+++ spring-boot-dao-jpa/src/main/java/com/yuan/spring/boot/dao/jpa/commons/service/BaseSerivce.java
@@ -0,0 +1,67 @@
+package com.yuan.spring.boot.dao.jpa.commons.service;
+
+import com.yuan.spring.boot.dao.jpa.commons.entity.po.BasePo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author yuane
+ * @date 2019/6/15 19:07
+ **/
+public interface BaseSerivce<T extends BasePo, ID extends Serializable> {
+
+    void checkSave(T t) throws RuntimeException;
+
+    void checkInsert(T t) throws RuntimeException;
+
+    void checkUpdate(T t) throws RuntimeException;
+
+    void save(T t);
+
+    void insert(T t);
+
+    void insertAll(T[] arrays);
+
+    void insertAll(Collection<T> collection);
+
+    void update(T t);
+
+    void updateAll(T[] arrays);
+
+    void updateAll(Collection<T> collection);
+
+    void saveAll(T[] ts);
+
+    void saveAll(Collection<T> collection);
+
+    void delete(ID id);
+
+    void delete(ID[] ids);
+
+    void delete(Collection<ID> collection);
+
+    Optional<T> findById(ID id);
+
+    List<T> findAllById(ID[] ids);
+
+    List<T> findAllById(Collection<ID> collection);
+
+    Optional<T> findOne(T t);
+
+    List<T> findAll();
+
+    List<T> findAll(T t);
+
+    List<T> findAll(T t, Sort sort);
+
+    Page<T> findAll(Pageable pageable);
+
+    Page<T> findAll(T t, Pageable pageable);
+
+}
