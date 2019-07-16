b5cab7a87eec18c3b46bf9e299a53d79a0ab70d1
sfadsfaf <werwqerwef>
2019-07-16 08:49:51 +0800

    没什么更新，就是修改下结构

---
+++ spring-boot-dao/spring-boot-dao-jooq/src/test/java/com/yuan/springbootwebjdbc/SpringBootWebJdbcApplicationTests.java
@@ -0,0 +1,25 @@
+package com.yuan.springbootwebjdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebJdbcApplicationTests {
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
