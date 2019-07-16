506cdf6db6da8127d650dd2c1b38ff1ddfcb831e
sfadsfaf <werwqerwef>
2019-07-16 16:22:10 +0800

    将项目和依赖拆分成独立部分，方便使用

--- spring-boot-system-apps/spring-boot-test-apps/spring-boot-test-app-nutz/src/main/java/com/yuan/spring/boot/test/app/nutz/dao/impl/SysUserDaoImpl.java
+++ spring-boot-system-apps/spring-boot-test-apps/spring-boot-test-app-nutz/src/main/java/com/yuan/spring/boot/test/app/nutz/dao/impl/SysUserDaoImpl.java
@@ -3,6 +3,7 @@ package com.yuan.spring.boot.test.app.nutz.dao.impl;
 import com.yuan.spring.boot.test.app.nutz.dao.SysUserDao;
 import com.yuan.spring.boot.test.app.nutz.entity.SysUser;
 import org.nutz.dao.Dao;
+import org.nutz.dao.sql.Sql;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +25,12 @@ public class SysUserDaoImpl implements SysUserDao {
         } else {
             dao.updateIgnoreNull(sysUser);
         }
+    }
+
+    public void delete(String id) {
+        Sql sql = dao.sqls().create("");
+        sql.setPager(dao.createPager(1, 1));
+        long aLong = sql.getLong(0);
     }
 
 }
