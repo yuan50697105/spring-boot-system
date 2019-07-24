f84c5d8b6dd06baea1eb5ed92ea76db15b2473be
袁恩志 <710575900@qq.com>
2019-07-24 23:45:23 +0800

    删除所有测试重新开始

--- spring-boot-system-apps/spring-boot-test-apps/spring-boot-shiro-apps/spring-boot-app3/src/main/java/com/spring/boot/test/app3/modules/commons/controller/BaseController.java
+++
@@ -1,9 +0,0 @@
-package com.spring.boot.test.app3.modules.commons.controller;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-@ControllerAdvice
-public abstract class BaseController extends com.yuan.spring.web.mvc.controller.BaseController {
-}
--- spring-boot-system-apps/spring-boot-test-apps/spring-boot-shiro-apps/spring-boot-app3/src/main/java/com/spring/boot/test/app3/modules/commons/service/BaseService.java
+++
@@ -1,7 +0,0 @@
-package com.spring.boot.test.app3.modules.commons.service;
-
-import com.spring.boot.test.app3.modules.commons.entity.domain.BaseEntity;
-import com.yuan.spring.boot.dao.mybatis.mapper.service.MybatisMapperService;
-
-public interface BaseService<T extends BaseEntity> extends MybatisMapperService<T, Long> {
-}
